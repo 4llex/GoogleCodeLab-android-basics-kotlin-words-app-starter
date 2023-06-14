7a0812af2909b4252c626c0ac461c4f7e7fe5c7f
Alex Rosa <alexr@gec.inatel.br>
2023-06-13 21:02:10 -0300

    added more tests and annotations

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,5 +1,6 @@
 package com.example.wordsapp
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
@@ -9,32 +10,74 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import junit.framework.TestCase.assertEquals
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
-    @Test
-    fun navigate_to_words_nav_component() {
-        val navController = TestNavHostController(
+    lateinit var navController: TestNavHostController
+    lateinit var letterListScenario: FragmentScenario<LetterListFragment>
+
+    @Before
+    fun setup(){
+
+        navController = TestNavHostController(
             ApplicationProvider.getApplicationContext()
         )
 
-        val letterListScenario =
-            launchFragmentInContainer<LetterListFragment>(themeResId = R.style.Theme_Words)
+        letterListScenario = launchFragmentInContainer(themeResId = R.style.Theme_Words)
 
         letterListScenario.onFragment { fragment ->
             navController.setGraph(R.navigation.nav_graph)
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
+    }
+
+    @Test
+    fun navigate_to_words_nav_component() {
         onView(withId(R.id.recycler_view))
             .perform(
                 RecyclerViewActions
                     .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
 
         assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
 
+    @Test
+    fun navigate_to_words_nav_component2() {
+        onView(withId(R.id.recycler_view))
+            .perform(
+                RecyclerViewActions
+                    .actionOnItemAtPosition<RecyclerView.ViewHolder>(4, click()))
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
+
+    @After
+    fun tearDownFunction() {
+        print("Tear down funtion")
+    }
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setupClass() {
+            print("setup class")
+        }
+        
+        @AfterClass
+        @JvmStatic
+        fun tearDownClass() {
+            print("tear down class")
+        }
     }
 
 }
